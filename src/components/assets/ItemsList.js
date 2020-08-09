75e24f0becc75fbc078a902da20204f3bb341cd8
HamedKh99 <goo.hamedacc@gmail.com>
2020-08-09 18:57:14 +0430

    properties screen filter init

--- src/components/assets/ItemsList.js
+++ src/components/assets/ItemsList.js
@@ -5,38 +5,14 @@ import { Container, Text } from "native-base";
 import ItemCard from "./ItemCard";
 
 const ItemsList = (props) => {
-  const DATA = [
-    {
-      id: "1",
-      title: "کوله",
-      image_url: "../../../assets/img/samples/bag1.jpg",
-      price: "18000"
-    },
-    {
-      id: "2",
-      title: "کوله",
-      image_url: "../../../assets/img/samples/bag2.jpg",
-      price: "10000"
-    },
-    {
-      id: "3",
-      title: "کوله",
-      image_url: "../../../assets/img/samples/bag.jpg",
-      price: "25000"
-    },
-    {
-      id: "4",
-      title: "کوله",
-      image_url: "../../../assets/img/samples/stick.jpg",
-      price: "30000"
-    },
-  ];
+
+  
 
   return (
     <Container style={{ backgroundColor: "transparent" }}>
       <FlatList
         style={{ marginBottom: 10 }}
-        data={DATA}
+        data={props.data}
         numColumns={2}
         renderItem={({ item }) => (
           <ItemCard
--- src/screens/PropertiesScreen.js
+++ src/screens/PropertiesScreen.js
@@ -4,6 +4,43 @@ import ItemsList from "../components/assets/ItemsList";
 import { ImageBackground, StyleSheet } from "react-native";
 
 export default class PropertiesScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedCategory : ""
+    }
+    this.DATA = [
+      {
+        id: "1",
+        title: "کوله",
+        image_url: "../../../assets/img/samples/bag1.jpg",
+        price: "18000",
+      },
+      {
+        id: "2",
+        title: "کوله",
+        image_url: "../../../assets/img/samples/bag2.jpg",
+        price: "10000",
+      },
+      {
+        id: "3",
+        title: "کوله",
+        image_url: "../../../assets/img/samples/bag.jpg",
+        price: "25000",
+      },
+      {
+        id: "4",
+        title: "کوله",
+        image_url: "../../../assets/img/samples/stick.jpg",
+        price: "30000",
+      },
+    ];
+    this.DATA_DICT = {}
+
+  }
+  
+
   render() {
     return (
       <Container>
@@ -11,7 +48,8 @@ export default class PropertiesScreen extends Component {
           source={require("../../assets/img/backgrounds/home.png")}
           style={styles.backgroundImageStyle}
         >
-          <ItemsList />
+          <Button />
+          <ItemsList data={this.DATA}/>
         </ImageBackground>
       </Container>
     );
