baa542df8167f3078640d85a8adb1aa57eb1a33d
pedramkho <pe.khorsandi@gmail.com>
2020-08-10 16:54:09 +0430

    Item Filter.

--- src/screens/PropertiesScreen.js
+++ src/screens/PropertiesScreen.js
@@ -1,45 +1,65 @@
 import React, { Component } from "react";
-import { Container, Text } from "native-base";
+import { Container, Text, Picker, Icon, View } from "native-base";
 import ItemsList from "../components/assets/ItemsList";
 import { ImageBackground, StyleSheet } from "react-native";
+import { Header } from "react-native/Libraries/NewAppScreen";
 
 export default class PropertiesScreen extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      selectedCategory : ""
+      selectedCategory : "همه"
     }
     this.DATA = [
       {
         id: "1",
         title: "کوله",
+        type: "کوله",
         image_url: "../../../assets/img/samples/bag1.jpg",
         price: "18000",
       },
       {
         id: "2",
         title: "کوله",
+        type: "کوله",
         image_url: "../../../assets/img/samples/bag2.jpg",
         price: "10000",
       },
       {
         id: "3",
         title: "کوله",
+        type: "کوله",
         image_url: "../../../assets/img/samples/bag.jpg",
         price: "25000",
       },
       {
         id: "4",
         title: "کوله",
+        type: "کوله",
         image_url: "../../../assets/img/samples/stick.jpg",
         price: "30000",
       },
     ];
-    this.DATA_DICT = {}
+    this.DATA_DICT = {"همه": this.DATA}
 
   }
   
+  onValueChange(value) {
+    this.setState({
+      selectedCategory: value
+    });
+  }
+
+  onMountData(){
+    for(var d in this.DATA){
+      if (d.type in this.DATA_DICT){
+        this.DATA_DICT[d.type].push(d)
+      }else{
+        this.DATA_DICT[d.type] = [d]
+      }
+    }
+  }
 
   render() {
     return (
@@ -48,8 +68,34 @@ export default class PropertiesScreen extends Component {
           source={require("../../assets/img/backgrounds/home.png")}
           style={styles.backgroundImageStyle}
         >
-          <Button />
-          <ItemsList data={this.DATA}/>
+          <View style={styles.picker}>
+            
+            <Picker
+                mode="dropdown"
+                placeholder="Select your SIM"
+                iosIcon={<Icon name="arrow-down" />}
+                placeholder="Select your SIM"
+                textStyle={{ color: "#5cb85c" }}
+                itemStyle={{
+                  backgroundColor: "#d3d3d3",
+                  marginLeft: 0,
+                  paddingLeft: 10
+                }}
+                itemTextStyle={{ color: '#788ad2' }}
+                style={{ width: undefined }}
+                selectedValue={this.state.selectedCategory}
+                onValueChange={this.onValueChange.bind(this)}
+              >
+                {
+                  Object.keys(this.DATA_DICT).map((key) => {return(<Picker.Item label={key} value={key}/>);})
+                }
+              </Picker>
+              
+          </View>
+
+          <View  style={styles.itemslist}>
+            <ItemsList data={this.DATA_DICT[this.state.selectedCategory]}/>
+          </View>
         </ImageBackground>
       </Container>
     );
@@ -65,4 +111,20 @@ const styles = StyleSheet.create({
     height: "100%",
     justifyContent: "center",
   },
+
+  picker: {
+    width: undefined,
+    justifyContent: "space-between",
+    flex: 1,
+    backgroundColor: '#00000022',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+    marginTop: 5
+  },
+
+  itemslist: {
+    flex: 15,
+  }
 });
