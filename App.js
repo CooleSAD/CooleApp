2ef81e173bde5ed32cd1443335ec5f1bc70f6138
HamedKh99 <goo.hamedacc@gmail.com>
2020-08-23 01:09:28 +0430

    ready to get apk

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
-import { AppLoading } from "expo";
+import { AppLoading, Notifications } from "expo";
 import { Container, Root } from "native-base";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import Constnants from "expo-constants";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+import { Platform } from "react-native";
 
 import AppNavigator from "./AppNavigator";
 import cooleApp from "./src/redux/reducers/index";
+
+
+// Notifications.setNotificationHandler({
+//   handleNotification: async () => ({
+//     shouldShowAlert: true,
+//     shouldPlaySound: false,
+//     shouldSetBadge: false,
+//   }),
+// });
 
 const store = createStore(cooleApp);
 
@@ -17,10 +28,15 @@ export default class App extends Component {
     super(props);
     this.state = {
       isReady: false,
+      notification : {}
     };
   }
 
   async componentDidMount() {
+    registerForPushNotificationsAsync().then((token) => console.log(token));
+
+    Notifications.addListener(this._handleNotification)
+
     await Font.loadAsync({
       Roboto: require("native-base/Fonts/Roboto.ttf"),
       Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
@@ -34,15 +50,19 @@ export default class App extends Component {
     this.setState({ isReady: true });
   }
 
+  _handleNotification = (notification) => {
+    console.log(notification)
+    this.setState({ notification: notification });
+  };
+
   render() {
     if (!this.state.isReady) {
       return <AppLoading />;
     }
 
     return (
-      <Provider store={store}> 
+      <Provider store={store}>
         <Container>
-
           <Root>
             <AppNavigator />
           </Root>
@@ -50,4 +70,36 @@ export default class App extends Component {
       </Provider>
     );
   }
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = await Notifications.getExpoPushTokenAsync();
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  // if (Platform.OS === 'android') {
+  //   Notifications.setNotificationChannelAsync('default', {
+  //     name: 'default',
+  //     importance: Notifications.AndroidImportance.MAX,
+  //     vibrationPattern: [0, 250, 250, 250],
+  //     lightColor: '#FF231F7C',
+  //   });
+  // }
+
+  return token;
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-export const BASE_ADDRESS = "http://192.168.1.7:8000"
+export const BASE_ADDRESS = "http://185.239.106.224"
 export const SIGNUP_API = BASE_ADDRESS + "/auth/users/"
 export const LOGIN_API = BASE_ADDRESS + "/auth/token/login/"
 export const LOGOUT_API = BASE_ADDRESS + "/auth/token/logout/"
